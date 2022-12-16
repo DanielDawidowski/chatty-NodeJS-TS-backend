5fc2b71a5267f420eb8689f5b21142fca88296ed
daniel <dvds1987@gmail.com>
2022-12-16 16:15:44 +0100

    Setup Eslint

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
-import dotenv from "dotenv";
-import bunyan from "bunyan";
+import dotenv from 'dotenv';
+import bunyan from 'bunyan';
 
 dotenv.config({});
 
@@ -12,20 +12,20 @@ class Config {
   public CLIENT_URL: string | undefined;
   public REDIS_HOST: string | undefined;
 
-  private readonly DEFAULT_DB_URL = "mongodb://127.0.0.1:27017/fullastack";
+  private readonly DEFAULT_DB_URL = 'mongodb://127.0.0.1:27017/fullastack';
 
   constructor() {
     this.DB_URL = process.env.DB_URL || this.DEFAULT_DB_URL;
-    this.JWT_TOKEN = process.env.JWT_TOKEN || "12345";
-    this.NODE_ENV = process.env.NODE_ENV || "";
-    this.SECRET_KEY_ONE = process.env.SECRET_KEY_ONE || "";
-    this.SECRET_KEY_TWO = process.env.SECRET_KEY_TWO || "";
-    this.CLIENT_URL = process.env.CLIENT_URL || "";
-    this.REDIS_HOST = process.env.REDIS_HOST || "";
+    this.JWT_TOKEN = process.env.JWT_TOKEN || '12345';
+    this.NODE_ENV = process.env.NODE_ENV || '';
+    this.SECRET_KEY_ONE = process.env.SECRET_KEY_ONE || '';
+    this.SECRET_KEY_TWO = process.env.SECRET_KEY_TWO || '';
+    this.CLIENT_URL = process.env.CLIENT_URL || '';
+    this.REDIS_HOST = process.env.REDIS_HOST || '';
   }
 
   public createLogger(name: string): bunyan {
-    return bunyan.createLogger({ name, level: "debug" });
+    return bunyan.createLogger({ name, level: 'debug' });
   }
 
   public validateConfig(): void {
--- src/setupDatabase.ts
+++ src/setupDatabase.ts
@@ -1,22 +1,22 @@
-import mongoose from "mongoose";
-import { config } from "./config";
-import Logger from "bunyan";
+import mongoose from 'mongoose';
+import { config } from './config';
+import Logger from 'bunyan';
 
-const log: Logger = config.createLogger("setupDatabase");
+const log: Logger = config.createLogger('setupDatabase');
 
 export default () => {
   const connect = () => {
     mongoose
       .connect(config.DB_URL!)
       .then(() => {
-        log.info("Successfully connected to database.");
+        log.info('Successfully connected to database.');
       })
       .catch((error) => {
-        log.error("Error connecting to database", error);
+        log.error('Error connecting to database', error);
         return process.exit(1);
       });
   };
-  mongoose.set("strictQuery", true);
+  mongoose.set('strictQuery', true);
   connect();
-  mongoose.connection.on("disconnected", connect);
+  mongoose.connection.on('disconnected', connect);
 };
