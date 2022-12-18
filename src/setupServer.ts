ffa6a49a8d27fb4d685fe3538bbedfa2ca02c967
daniel <dvds1987@gmail.com>
2022-12-18 14:11:57 +0100

    Change absolute imports

--- src/setupServer.ts
+++ src/setupServer.ts
@@ -1,4 +1,3 @@
-import { CustomError, IErrorResponse } from './shared/globals/helpers/error-handler';
 import { Application, json, urlencoded, Response, Request, NextFunction } from 'express';
 import http from 'http';
 import cors from 'cors';
@@ -10,10 +9,11 @@ import compression from 'compression';
 import { Server } from 'socket.io';
 import { createClient } from 'redis';
 import { createAdapter } from '@socket.io/redis-adapter';
-import 'express-async-errors';
-import { config } from './config';
-import applicationRoutes from './routes';
 import Logger from 'bunyan';
+import 'express-async-errors';
+import { config } from '@root/config';
+import applicationRoutes from '@root/routes';
+import { CustomError, IErrorResponse } from '@global/helpers/error-handler';
 
 const SERVER_PORT = 5000;
 
@@ -110,5 +110,7 @@ export class ChattyServer {
     });
   }
 
-  private socketIOConnections(io: Server): void {}
+  private socketIOConnections(io: Server): void {
+    log.info('socketIOConnections');
+  }
 }
