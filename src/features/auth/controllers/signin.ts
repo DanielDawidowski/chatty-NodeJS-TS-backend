3bde4312ce6219f2d6deefc60464d254d49fdcf8
daniel <dvds1987@gmail.com>
2023-02-28 13:32:59 +0100

    User login controller

---
+++ src/features/auth/controllers/signin.ts
@@ -0,0 +1,39 @@
+import { Request, Response } from 'express';
+import JWT from 'jsonwebtoken';
+import HTTP_STATUS from 'http-status-codes';
+import { config } from '@root/config';
+import { joiValidation } from '@global/decorators/joi-validation.decorators';
+import { BadRequestError } from '@global/helpers/error-handler';
+import { authService } from '@service/db/auth.service';
+import { loginSchema } from '@auth/schemes/signin';
+import { IAuthDocument } from '@auth/interfaces/auth.interface';
+
+export class SignIn {
+  @joiValidation(loginSchema)
+  public async read(req: Request, res: Response): Promise<void> {
+    const { email, password } = req.body;
+
+    const existingUser: IAuthDocument = await authService.getUserByEmail(email);
+
+    if (!existingUser) {
+      throw new BadRequestError('Invalid credentials');
+    }
+
+    const passwordsMatch: boolean = await existingUser.comparePassword(password);
+    if (!passwordsMatch) {
+      throw new BadRequestError('Invalid credentials');
+    }
+    const userJwt: string = JWT.sign(
+      {
+        userId: existingUser._id,
+        uId: existingUser.uId,
+        email: existingUser.email,
+        username: existingUser.username,
+        avatarColor: existingUser.avatarColor
+      },
+      config.JWT_TOKEN!
+    );
+    req.session = { jwt: userJwt };
+    res.status(HTTP_STATUS.OK).json({ message: 'User login successfully', user: existingUser, token: userJwt });
+  }
+}
--- src/shared/services/db/auth.service.ts
+++ src/shared/services/db/auth.service.ts
@@ -3,6 +3,7 @@ import { AuthModel } from '@auth/models/auth.schema';
 import { Helpers } from '@global/helpers/helpers';
 
 class AuthService {
+  getAuthUserByUsername: any;
   public async createAuthUser(data: IAuthDocument): Promise<void> {
     await AuthModel.create(data);
   }
@@ -12,6 +13,11 @@ class AuthService {
       $or: [{ username: Helpers.firstLetterUppercase(username) }, { email: Helpers.lowerCase(email) }]
     };
     const user: IAuthDocument = (await AuthModel.findOne(query).exec()) as IAuthDocument;
+    return user;
+  }
+
+  public async getUserByEmail(email: string): Promise<IAuthDocument> {
+    const user: IAuthDocument = (await AuthModel.findOne({ email: Helpers.lowerCase(email) }).exec()) as IAuthDocument;
     return user;
   }
 }
