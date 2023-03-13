d5f20dc9b93325001eb138d4d8eccbe3f118e806
daniel <dvds1987@gmail.com>
2023-03-13 16:10:30 +0100

    Reset password controller method

--- src/shared/services/queues/email.queue.ts
+++ src/shared/services/queues/email.queue.ts
@@ -5,7 +5,7 @@ import { emailWorker } from '@worker/email.worker';
 class EmailQueue extends BaseQueue {
   constructor() {
     super('email');
-    this.processJob('forgotPassword', 5, emailWorker.addNotificationEmail);
+    this.processJob('forgotPasswordEmail', 5, emailWorker.addNotificationEmail);
   }
   public addEmailJob(name: string, data: IEmailJob): void {
     this.addJob(name, data);
