62f774a15f078431168f031b9628cfe79d0cf8ee
daniel <dvds1987@gmail.com>
2023-03-15 18:28:28 +0100

    Add post reaction to redis cache

---
+++ src/shared/services/redis/reaction.cache.ts
@@ -0,0 +1,41 @@
+import { BaseCache } from '@service/redis/base.cache';
+import Logger from 'bunyan';
+// import { find } from 'lodash';
+import { config } from '@root/config';
+import { ServerError } from '@global/helpers/error-handler';
+import { IReactionDocument, IReactions } from '@reaction/interfaces/reaction.interface';
+// import { Helpers } from '@global/helpers/helpers';
+
+const log: Logger = config.createLogger('reactionsCache');
+
+export class ReactionCache extends BaseCache {
+  constructor() {
+    super('reactionsCache');
+  }
+
+  public async savePostReactionToCache(
+    key: string,
+    reaction: IReactionDocument,
+    postReactions: IReactions,
+    type: string,
+    previousReaction: string
+  ): Promise<void> {
+    try {
+      if (!this.client.isOpen) {
+        await this.client.connect();
+      }
+
+      if (previousReaction) {
+        // this.removePostReactionFromCache(key, reaction.username, postReactions);
+      }
+
+      if (type) {
+        await this.client.LPUSH(`reactions:${key}`, JSON.stringify(reaction));
+        await this.client.HSET(`posts:${key}`, 'reactions', JSON.stringify(postReactions));
+      }
+    } catch (error) {
+      log.error(error);
+      throw new ServerError('Server error. Try again.');
+    }
+  }
+}
