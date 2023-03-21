e0937d9d61b1e72e1d4481d08055dc9946d9ddba
daniel <dvds1987@gmail.com>
2023-03-21 13:17:09 +0100

    Notification socketIO handler

--- src/setupServer.ts
+++ src/setupServer.ts
@@ -17,6 +17,7 @@ import { CustomError, IErrorResponse } from '@global/helpers/error-handler';
 import { SocketIOPostHandler } from '@socket/post';
 import { SocketIOFollowerHandler } from '@socket/follower';
 import { SocketIOUserHandler } from '@socket/user';
+import { SocketIONotificationHandler } from '@socket/notifications';
 
 const SERVER_PORT = 5000;
 
@@ -117,9 +118,11 @@ export class ChattyServer {
     const postSocketHandler: SocketIOPostHandler = new SocketIOPostHandler(io);
     const followerSocketHandler: SocketIOFollowerHandler = new SocketIOFollowerHandler(io);
     const socketIOUserObject: SocketIOUserHandler = new SocketIOUserHandler(io);
+    const notificationSocketHandler: SocketIONotificationHandler = new SocketIONotificationHandler();
 
     postSocketHandler.listen();
     followerSocketHandler.listen();
     socketIOUserObject.listen();
+    notificationSocketHandler.listen(io);
   }
 }
