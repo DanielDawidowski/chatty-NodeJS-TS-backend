b93fd2726ad0fd55eb60b980c68208bef325a080
daniel <dvds1987@gmail.com>
2023-03-21 18:42:42 +0100

    Get images controller

--- src/features/images/routes/imageRoutes.ts
+++ src/features/images/routes/imageRoutes.ts
@@ -2,7 +2,7 @@ import express, { Router } from 'express';
 import { authMiddleware } from '@global/helpers/auth-middleware';
 import { Add } from '@image/controllers/add-image';
 import { Delete } from '@image/controllers/delete-image';
-// import { Get } from '@image/controllers/get-images';
+import { Get } from '@image/controllers/get-images';
 
 class ImageRoutes {
   private router: Router;
@@ -14,7 +14,7 @@ class ImageRoutes {
   public routes(): Router {
     this.router.post('/images/profile', authMiddleware.checkAuthentication, Add.prototype.profileImage);
     this.router.post('/images/background', authMiddleware.checkAuthentication, Add.prototype.backgroundImage);
-    // this.router.get('/images/:userId', authMiddleware.checkAuthentication, Get.prototype.images);
+    this.router.get('/images/:userId', authMiddleware.checkAuthentication, Get.prototype.images);
     this.router.delete('/images/:imageId', authMiddleware.checkAuthentication, Delete.prototype.image);
     this.router.delete('/images/background/:bgImageId', authMiddleware.checkAuthentication, Delete.prototype.backgroundImage);
 
