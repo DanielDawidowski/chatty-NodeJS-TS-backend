e7559def0edf03adc6edea2ca2c85d724cbc92e7
daniel <dvds1987@gmail.com>
2023-03-22 12:45:16 +0100

    Add chat users to redis cache

--- src/features/chat/routes/chatRoutes.ts
+++ src/features/chat/routes/chatRoutes.ts
@@ -17,8 +17,8 @@ class ChatRoutes {
     this.router.post('/chat/message', authMiddleware.checkAuthentication, Add.prototype.message);
     // this.router.get('/chat/message/conversation-list', authMiddleware.checkAuthentication, Get.prototype.conversationList);
     // this.router.get('/chat/message/user/:receiverId', authMiddleware.checkAuthentication, Get.prototype.messages);
-    // this.router.post('/chat/message/add-chat-users', authMiddleware.checkAuthentication, Add.prototype.addChatUsers);
-    // this.router.post('/chat/message/remove-chat-users', authMiddleware.checkAuthentication, Add.prototype.removeChatUsers);
+    this.router.post('/chat/message/add-chat-users', authMiddleware.checkAuthentication, Add.prototype.addChatUsers);
+    this.router.post('/chat/message/remove-chat-users', authMiddleware.checkAuthentication, Add.prototype.removeChatUsers);
     // this.router.put('/chat/message/mark-as-read', authMiddleware.checkAuthentication, Update.prototype.message);
     // this.router.put('/chat/message/reaction', authMiddleware.checkAuthentication, Message.prototype.reaction);
     // this.router.delete(
