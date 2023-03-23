3fc2e5fc39024a36d0de93cf70200389b4f3a733
daniel <dvds1987@gmail.com>
2023-03-23 16:15:51 +0100

    Change password

--- src/features/user/routes/userRoutes.ts
+++ src/features/user/routes/userRoutes.ts
@@ -2,7 +2,7 @@ import express, { Router } from 'express';
 import { authMiddleware } from '@global/helpers/auth-middleware';
 import { Get } from '@user/controllers/get-profile';
 import { Search } from '@user/controllers/search-user';
-// import { Update } from '@user/controllers/change-password';
+import { Update } from '@user/controllers/change-password';
 // import { Edit } from '@user/controllers/update-basic-info';
 // import { UpdateSettings } from '@user/controllers/update-settings';
 
@@ -21,7 +21,7 @@ class UserRoutes {
     this.router.get('/user/profile/user/suggestions', authMiddleware.checkAuthentication, Get.prototype.randomUserSuggestions);
     this.router.get('/user/profile/search/:query', authMiddleware.checkAuthentication, Search.prototype.user);
 
-    // this.router.put('/user/profile/change-password', authMiddleware.checkAuthentication, Update.prototype.password);
+    this.router.put('/user/profile/change-password', authMiddleware.checkAuthentication, Update.prototype.password);
     // this.router.put('/user/profile/basic-info', authMiddleware.checkAuthentication, Edit.prototype.info);
     // this.router.put('/user/profile/social-links', authMiddleware.checkAuthentication, Edit.prototype.social);
     // this.router.put('/user/profile/settings', authMiddleware.checkAuthentication, UpdateSettings.prototype.notification);
